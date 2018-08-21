61e945490725130b024a8f5e4a9297c4bd899c1a
Yen Truong <truthblue82@gmail.com>
2018-08-21 22:59:45 +0700

    add favicon

--- wp-content/themes/alchem-pro/header.php
+++ wp-content/themes/alchem-pro/header.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-        <link rel="icon" href="<?php echo TEMPLATEPATH;?>/alchem-pro/images/favico.ico" type="image/x-icon"/>
-        <link rel="shortcut icon" href="<?php echo TEMPLATEPATH;?>/alchem-pro/images/favico.ico" type="image/x-icon"/>
+        <link rel="icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/favico.ico" type="image/x-icon"/>
+        <link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/favico.ico" type="image/x-icon"/>
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
